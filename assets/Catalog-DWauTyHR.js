d63805aedcc55beb66e9d2f59c8c9c5a9fd2c497
Sergii-Drozdiuk <drozduyk@gmail.com>
2024-06-19 10:39:54 +0300

    Updates

---
+++ assets/Catalog-DWauTyHR.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BbQ88hrr.js";const t=()=>a.jsx("main",{className:"grow",children:a.jsx("p",{children:"Catalog page"})});function e(){return a.jsx(t,{})}export{e as default};
