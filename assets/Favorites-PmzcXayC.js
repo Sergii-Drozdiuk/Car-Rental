9de7d6b9430e4dcc6f58a656f8b0cd7ec4b8b329
Sergii-Drozdiuk <drozduyk@gmail.com>
2024-06-19 09:44:33 +0300

    Updates

---
+++ assets/Favorites-PmzcXayC.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Bx1LzzNj.js";const e=()=>r.jsx("main",{className:"grow",children:r.jsx("p",{children:"Favorites page"})});function t(){return r.jsx(e,{})}export{t as default};
