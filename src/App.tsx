6c2491ecaf5963d9de1e0a391d906f9d5c34b0a9
Sergii-Drozdiuk <drozduyk@gmail.com>
2024-06-19 16:02:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 // import { store, persistor } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Car-rental">
+    <HashRouter basename="/Car-rental">
       {/* <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}> */}
       <App />
       {/* </PersistGate>
       </Provider> */}
-    </BrowserRouter>
+    </HashRouter >
   </React.StrictMode>
 );
+
+// basename="/Car-rental"
